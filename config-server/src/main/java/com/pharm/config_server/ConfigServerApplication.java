4f9e6cf3d43878dfb719275d42259ddb33ab3f79
Kasthoori <ap.kasth@gmail.com>
2025-11-23 19:47:40 +1300

    add config server

---
+++ config-server/src/main/java/com/pharm/config_server/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.pharm.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
